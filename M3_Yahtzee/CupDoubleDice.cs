e9281e1d4062eb78ab95e8612e92ba952cc0ca90
Martin <martin_lenart@icloud.com>
2024-11-20 17:51:26 +0900

    initial commit

---
+++ M3_Yahtzee/CupDoubleDice.cs
@@ -0,0 +1,8 @@
+﻿namespace M3_Yahtzee
+{
+    class CupDoubleDice : CupOfDice, ICupDoubleDice
+    {
+        public bool IsPair => this[0] == this[1];
+        public CupDoubleDice() : base(2) { }
+    }
+}
